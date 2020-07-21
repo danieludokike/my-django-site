c15182cf28b08e9495fd4516bff5f82cdd86a2e9
DANIELA1DIV <daniela1div@localhost.localdomain>
2020-07-21 08:40:25 +0100

    last commit file on Tue Jul 21, 8:40:23:PM

--- mysite/mywebpage/urls.py
+++ mysite/mywebpage/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path
 
 from .views import (
-        home_page_view
+        home_page_view,
+        contact_me_page_view,
+        about_me_page_view,
+        portfolio_page_view,
     )
 
 
+app_name = 'mywebpage'
 urlpatterns = [
     path('', home_page_view, name='home'),
+    path('contact/', contact_me_page_view, name='contact'),
+    path('about/', about_me_page_view, name='about'),
+    path('portfolio/', portfolio_page_view, name='portfolio'),
 ]
--- mysite/mywebpage/views.py
+++ mysite/mywebpage/views.py
@@ -1,9 +1,72 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
-# Create your views here.
+from .models import UserContactForm
+
+from .forms import (
+        ContactMeForm
+    )
+
+import re  # Regular expression module
 
 
 def home_page_view(request):
     """Renders the home page on request"""
-    return HttpResponse("<h1>Hello World!!! Welcome to my home page</h1>")
+    template = 'mywebpage-app/index.html'
+
+    context = {
+        # Sending data to the page and requested objects
+
+    }
+    return render(request, template, context)
+
+
+def contact_me_page_view(request):
+    """Renders the contact me page on request"""
+    if request.method == 'POST':
+        full_name = request.POST['full_name']
+        email = request.POST['email']
+        subject = request.POST['subject']
+        text = request.POST['text']
+
+        user_passed_details = UserContactForm(
+            full_name=full_name,
+            email=email,
+            subject=subject,
+            text=text,
+        )
+        user_passed_details.save()
+        submission_successful_message = 'Hello ' + full_name + ' thanks for contacting us'
+        messages.info(request, submission_successful_message)
+
+        return redirect('mywebpage:contact')
+
+    else:
+        template = 'mywebpage-app/contact.html'
+
+        context = {
+            # Sending date to page and requested objects
+            'form': ContactMeForm
+        }
+        return render(request, template, context)
+
+
+def about_me_page_view(request):
+    """Renders the about me page"""
+    template = 'mywebpage-app/about-me.html'
+
+    context = {
+        # Sending data and requested objects to the page
+    }
+    return render(request, template, context)
+
+
+def portfolio_page_view(request):
+    """Renders the portfolio page"""
+    template = 'mywebpage-app/portfolio.html'
+
+    context = {
+        # Sending data and requested objects to the page
+    }
+    return render(request, template, context)
