8c1b9f8c199c269ce419abea4daf689841ed519c
DANIELA1DIV <daniela1div@localhost.localdomain>
2020-07-21 07:09:07 +0100

    This commit was made recently on mysite files

---
+++ mysite/mywebpage/forms.py
@@ -0,0 +1,42 @@
+from django import forms
+
+
+class ContactMeForm(forms.Form):
+	full_name = forms.CharField(
+		max_length=200,
+		widget=forms.TextInput(
+			attrs={
+				'placeholder': 'FULL NAME',
+				'class': 'full_name_input',
+			}
+		)
+	)
+
+	email = forms.CharField(
+		max_length=200,
+		widget=forms.EmailInput(
+			attrs={
+				'placeholder': 'EMAIL ADDRESS',
+				'class': 'email_input',
+			}
+		)
+	)
+
+	subject = forms.CharField(
+		max_length=200,
+		widget=forms.TextInput(
+			attrs={
+				'placeholder': 'ENTER SUBJECT',
+				'class': 'subject_input',
+			}
+		)
+	)
+
+	text = forms.CharField(
+		widget=forms.Textarea(
+			attrs={
+				'placeholder': 'Leave Us a Message Here!!',
+				'class': 'text_input',
+			}
+		)
+	)
--- mysite/mywebpage/models.py
+++ mysite/mywebpage/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class UserContactForm(models.Model):
+    full_name = models.CharField(max_length=200)
+    email = models.EmailField(max_length=200)
+    subject = models.CharField(max_length=200)
+    text = models.TextField(blank=False)
+
+    def __str__(self):
+        return self.full_name
